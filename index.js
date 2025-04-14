182e5e757fd1b38df04213eef64b77c36197e8fc
larryonilenla <larryonilenla@gmail.com>
2025-04-14 07:39:01 -0600

    Add eval endpoint to test CodeQL

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  eval(req.query.code); // SECURITY RISK
+  res.send('Evaluated!');
 });
 
 // Intentionally add a minor security flaw for CodeQL to detect (e.g., no rate limiting)
